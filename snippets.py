cfd4d71499ad3b6724f881296461e3c9c309c82d
Tony Hoang <tonyhoang314@gmail.com>
2017-04-20 19:03:06 +0000

    v2

--- snippets.py
+++ snippets.py
@@ -1,23 +1,52 @@
 import logging
+import argparse
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
-def put(name, snippet)
+def put(name, snippet):
     """
     Store a snippet with an associated name.
     
     Returns the name and the snippet
     """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r}").format(name, snippet)
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
     return name, snippet
     
 def get(name):
     """Retrieve the snippet with a given name.
     
-    If there is no such snippet, return '404: Snipper Not Found'.
+    If there is no such snippet, return '404: Snippet Not Found'.
     
     Returns the snippet.
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
+    
+def delete(name):
+    """deletes the snippet with a given name.
+    
+    If there is no such snippet, return '404: Snippet Not Found'.
+    
+    Returns the snippet.
+    """
+    logging.error("FIXME: Unimplemented - delete({!r})".format(name))
+    return ""
+    
+def patch(name, snippet):
+    """
+    Updates the snippet with the associated name.
+    
+    Returns the name and the snippet
+    """
+    logging.error("FIXME: Unimplemented - patch({!r}, {!r})".format(name, snippet))
+    return name, snippet
+    
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args()
+    
+if __name__ == "__main__":
+    main()
